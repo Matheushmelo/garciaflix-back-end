734d2d61944228a2fb64020653f924be4269b75b
Matheus Henrique <104454290+Matheushmelo@users.noreply.github.com>
2024-05-07 05:27:36 -0300

    Obtendo cursos de uma categoria na API

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -8,8 +8,20 @@ export const categoriesController = {
 
     try {
       const paginatedCategories = await categoryService.findAllPaginated(page, perPage)
-      
       return res.json(paginatedCategories)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  show: async (req: Request, res: Response) => {
+    const { id } = req.params
+
+    try {
+      const category = await categoryService.findByIdWithCourses(id)
+      return res.json(category)
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message })
--- src/models/index.ts
+++ src/models/index.ts
@@ -3,7 +3,7 @@ import { Course } from "./Courses";
 import { Episode } from "./Episode";
 import { User } from "./User";
 
-Category.hasMany(Course)
+Category.hasMany(Course, { as: 'courses' })
 
 Course.belongsTo(Category)
 Course.hasMany(Episode)
