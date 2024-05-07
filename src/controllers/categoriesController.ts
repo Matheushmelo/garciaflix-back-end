6430f5393c4faf857c3dea5168c2453834b2a9bb
Matheus Henrique <104454290+Matheushmelo@users.noreply.github.com>
2024-05-07 04:51:03 -0300

    Adicionando paginação de categorias na API

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -1,24 +1,19 @@
 import { Request, Response } from "express";
-import { Category } from "../models";
+import { categoryService } from "../services/categoryService";
+import { getPaginationParams } from "../helpers/getPaginationParams";
 
 export const categoriesController = {
   index: async (req: Request, res: Response) => {
+    const [ page, perPage ] = getPaginationParams(req.query)
 
     try {
-
-      const categories = await Category.findAll({
-        attributes: ['id', 'name', 'position'],
-        order: [['position', 'ASC']]
-      })
-  
-      return res.json(categories)
-
+      const paginatedCategories = await categoryService.findAllPaginated(page, perPage)
+      
+      return res.json(paginatedCategories)
     } catch (err) {
-
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message })
       }
-      
     }
   }
 }
