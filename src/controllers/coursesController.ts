c76a05920176042c6ab7ce1192291a269cc211cd
Matheus Henrique <104454290+Matheushmelo@users.noreply.github.com>
2024-05-09 08:30:31 -0300

    Configurando endpoint para fazer busca por cursos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { courseService } from "../services/courseService";
+import { getPaginationParams } from "../helpers/getPaginationParams";
 
 export const coursesController = {
   featured: async (req: Request, res: Response) => {
@@ -16,6 +17,20 @@ export const coursesController = {
     try {
       const newestCourses = await courseService.getTop10Newest()
       return res.json(newestCourses)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+  search: async (req: Request, res: Response) => {
+    const { name } = req.query
+    const [page, perPage] = getPaginationParams(req.query)
+
+    try {
+      if(typeof name !== 'string') throw new Error('Name param must be of type string')
+      const courses = await courseService.findByName(name, page, perPage)  
+      return res.json(courses)
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message })
