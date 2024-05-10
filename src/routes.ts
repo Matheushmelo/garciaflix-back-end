3915b60ad418250c11e9cab7d3b15f7e9aed7b92
Matheus Henrique <104454290+Matheushmelo@users.noreply.github.com>
2024-05-10 05:32:42 -0300

    Configurando endpoint para registro e autenticação de usuário

--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,11 @@ import express from 'express'
 import { categoriesController } from './controllers/categoriesController'
 import { coursesController } from './controllers/coursesController'
 import { episodesController } from './controllers/episodesConctroller'
+import { authController } from './controllers/authController'
 
 const router = express.Router()
+
+router.post('/auth/register', authController.register)
 
 router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,13 @@ import { router } from "./routes"
 
 const app = express()
 
+app.use(express.static('public'))
+
+app.use(express.json())
+
 app.use(adminJs.options.rootPath, adminJsRouter)
 
 app.use(router)
-
-app.use(express.static('public'))
 
 const PORT = process.env.PORT || 3000
 
