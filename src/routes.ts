9aeadcfb61d86451ef7c746ce035b4fc9e66e80a
Matheus Henrique <104454290+Matheushmelo@users.noreply.github.com>
2024-05-13 05:36:17 -0300

    Criando endpoint para adicionar cursos como favoritos

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { coursesController } from './controllers/coursesController'
 import { episodesController } from './controllers/episodesConctroller'
 import { authController } from './controllers/authController'
 import { ensureAuth, ensureAuthVideoQuery } from './middlewares/auth'
+import { favoritesController } from './controllers/favoritesController'
 
 const router = express.Router()
 
@@ -19,5 +20,7 @@ router.get('/courses/search', ensureAuth, coursesController.search)
 router.get('/courses/:id', ensureAuth, coursesController.show)
 
 router.get('/episodes/stream', ensureAuthVideoQuery, episodesController.stream)
+
+router.post('/favorites', ensureAuth, favoritesController.save)
 
 export { router }
