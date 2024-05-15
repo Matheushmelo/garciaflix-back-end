72ecffe1fa6a993bfbe578a9796d430172554413
Matheus Henrique <104454290+Matheushmelo@users.noreply.github.com>
2024-05-15 07:11:18 -0300

    Atualizando endpoint de cursos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { courseService } from "../services/courseService";
 import { getPaginationParams } from "../helpers/getPaginationParams";
+import { AuthenticatedRequest } from "../middlewares/auth";
+import { likeService } from "../services/likeService";
+import { favoriteService } from "../services/favoriteService";
 
 export const coursesController = {
   featured: async (req: Request, res: Response) => {
@@ -37,12 +40,20 @@ export const coursesController = {
       }
     }
   },
-  show: async (req: Request, res: Response) => {
-    const { id } = req.params
+  show: async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user!.id
+    const courseId = req.params.id
 
     try {
-      const course = await courseService.findByIdWithEpisodes(id)
-      return res.json(course)
+      const course = await courseService.findByIdWithEpisodes(courseId)
+
+      if (!course) return res.status(404).json({ message: 'Curso não encontrado' })
+      
+      const liked = await likeService.isLiked(userId, Number(courseId))
+      const favorited = await favoriteService.isFavorited(userId, Number(courseId))
+
+      return res.json({ ...course.get(), favorited, liked })
+      
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message })
--- src/services/likeService.ts
+++ src/services/likeService.ts
@@ -16,5 +16,16 @@ export const likeService = {
         courseId
       }
     })
+  },
+
+  isLiked: async (userId: number, courseId: number) => {
+    const like = await Like.findOne({
+      where: {
+        userId,
+        courseId
+      }
+    })
+
+    return like !== null ? true : false
   }
 }
